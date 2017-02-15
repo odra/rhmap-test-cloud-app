1b40ee9873ded395a477e322fb59fa7bd56784c4
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2017-02-15 15:46:37 +0100

    resets log file on every start and saves it to public dir

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const winston = require("winston");
+const fs = require("fs");
+
+const PATH_LOG_FILE = "public/push.log";
 
 class LoggerHelper {
 
     static init() {
-        winston.add(winston.transports.File, { filename: `${Date.now()}.log` });
+        fs.unlinkSync(PATH_LOG_FILE);
+        winston.add(winston.transports.File, { filename: PATH_LOG_FILE });
         winston.remove(winston.transports.Console);
     }
 
